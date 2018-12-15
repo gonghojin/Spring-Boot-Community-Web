abaa7d80219bdbad9fca9c5aa1c6cec9879deec9
gonghojin <yolip51@naver.com>
2018-12-15 23:48:54 +0900

    Add : list.html with thymeleaf

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -22,13 +22,13 @@ public class BoardController {
     public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
         model.addAttribute("board", boardService.findBoardByIdx(idx));
 
-        return "/board/form";
+        return "board/form";
     }
 
     @GetMapping("/list")
     public String list(@PageableDefault Pageable pageable, Model model) {
         model.addAttribute("boardList", boardService.findBoardList(pageable));
 
-        return "/board/list";
+        return "board/list";
     }
 }

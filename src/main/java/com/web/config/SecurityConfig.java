5234e5244b9b8b0bd8768d6cc67bd8a3917c0209
gonghojin <yolip51@naver.com>
2018-12-19 01:45:36 +0900

    Add: Oauth Config In SecurityConfig, UserTokenService

--- src/main/java/com/web/config/SecurityConfig.java
+++ src/main/java/com/web/config/SecurityConfig.java
@@ -1,19 +1,43 @@
 package com.web.config;
 
+import com.web.domain.enums.SocialType;
 import com.web.oauth.ClientResources;
+import com.web.oauth.UserTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.CompositeFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.web.domain.enums.SocialType.FACEBOOK;
+import static com.web.domain.enums.SocialType.GOOGLE;
+import static com.web.domain.enums.SocialType.KAKAO;
 
 @Configuration
 @EnableWebSecurity
+@EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2ClientContext oauth2ClientContext;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -41,7 +65,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
             .and()
                 .addFilterBefore(filter, CsrfFilter.class) // 첫번째 인자보다 먼저 시작될 필터를 등록 즉, 문자 인코딩 필터보다 CsrfFilter가 먼저
+                .addFilterBefore(oauth2Filter(), BasicAuthenticationFilter.class)
                 .csrf().disable();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+
+        return registration;
+    }
+
+    private Filter oauth2Filter() {
+        List<Filter> filters = new ArrayList<>();
+        filters.add(oauth2Filter(facebook(), "/login/facebook", FACEBOOK));
+        filters.add(oauth2Filter(google(), "/login/google", GOOGLE));
+        filters.add(oauth2Filter(kakao(), "/login/kakao", KAKAO));
+
+        CompositeFilter filter = new CompositeFilter();
+        filter.setFilters(filters);
+
+        return filter;
+    }
+
+    private Filter oauth2Filter(ClientResources client, String path, SocialType socialType) {
+        OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+
+        OAuth2ClientAuthenticationProcessingFilter filter =
+                new OAuth2ClientAuthenticationProcessingFilter(path);
+       filter.setRestTemplate(template);
+       filter.setTokenServices(new UserTokenService(client, socialType));
+       filter.setAuthenticationSuccessHandler((request, response, authentication)
+            -> response.sendRedirect("/" + socialType.getValue() + "/complete"));
+        filter.setAuthenticationFailureHandler((request, response, exception) -> response.sendRedirect("/error"));
+        return filter;
     }
 
     @Bean
